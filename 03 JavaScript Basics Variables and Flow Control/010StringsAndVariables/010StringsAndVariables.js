4bb43cbeb035d8749badf1f81795bdee42f106b6
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-08 17:44:56 -0400

    finished 010 strings and variables, MacBookPro 10/08/2022

---
+++ 03 JavaScript Basics Variables and Flow Control/010StringsAndVariables/010StringsAndVariables.js
@@ -0,0 +1,12 @@
+'use strict';
+let firstName = 'Andrew';
+let lastName = 'Mead';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+let city = 'Fort Lauderdale';
+let country = 'United States';
+let locations = city + ', ' + country;
+
+console.log(locations);
