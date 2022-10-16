ff5337331a8331a256cfa721420059bb3da8ce6a
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-16 13:29:22 -0400

    updating 013 Build a Temperature Converter, MacBookPro 10/16/2022 #1

--- 03 JavaScript Basics Variables and Flow Control/013 Build a Temperature Converter/013 Build a Temperature Converter.js
+++ 03 JavaScript Basics Variables and Flow Control/013 Build a Temperature Converter/013 Build a Temperature Converter.js
@@ -1,7 +1,12 @@
 'use strict';
 let fahrenheit = 32; // 0c - 273.15k
+let celsius = (fahrenheit - 32) * (5 / 9);
+let kelvin = (fahrenheit + 459.67) * (5 / 9);
 // calculate celsius and store in celsius variable
 // print that value
 
 // calculate kelvin value and store in variable
 // print that value
+
+console.log(celsius);
+console.log(kelvin);
