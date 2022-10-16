5697596bfa71a5340dce2d677c4c6f757d2e035e
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-16 14:25:05 -0400

    updating 015 If Statements, MacBookPro 10/16/2022 #1

---
+++ 03 JavaScript Basics Variables and Flow Control/script.old.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const calcAge = function (birthYear) {
+  return 2021 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired `);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1990, "Jonas"));
+console.log(yearsUntilRetirement(1984, "Mike"));
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     return retirement;
+//   } else {
+//     return -1;
+//   }
+// };
+
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
