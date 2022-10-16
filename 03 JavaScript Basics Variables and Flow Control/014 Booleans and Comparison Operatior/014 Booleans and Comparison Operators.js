af479f9b9a7bb29e939c422c0a47c147b9924d42
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-16 14:14:36 -0400

    finished 014 Booleans and Comparison Operators, MacBookPro 10/16/2022

--- 03 JavaScript Basics Variables and Flow Control/014 Booleans and Comparison Operatior/014 Booleans and Comparison Operators.js
+++ 03 JavaScript Basics Variables and Flow Control/014 Booleans and Comparison Operatior/014 Booleans and Comparison Operators.js
@@ -1,1 +1,30 @@
 'use strict';
+/// ===  -> equality operator
+/// !==  -> not equal opertor
+/// <  -> less than Operator
+/// >  -> Greater than Opeartor
+/// <=  -> less than or equal to Operator
+/// >=  -> Greater than or equal to Opeartor
+
+let temp = 31;
+let isFreezing1 = temp === 31;
+isFreezing1 = temp !== 32;
+isFreezing1 = temp < 32;
+isFreezing1 = temp > 32;
+isFreezing1 = temp <= 32;
+isFreezing1 = temp >= 32;
+
+console.log(isFreezing1);
+
+/// Challenge Area
+
+/// Create age set to Your age
+let age = 38;
+/// Calculate is child - if they are 7 or under
+let isChild = age <= 7;
+/// Calculate is senior - if they are 65 or older
+let isSenior = age >= 65;
+/// Print is child value
+console.log(isChild);
+/// Print is senior value
+console.log(isSenior);
