53f94e68ad1c176c73f3b4d96a0f21b2ab6b05dc
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-16 15:32:31 -0400

    updating 016 Advanced If statements, MacBookPro 10/16/2022 #1

--- 03 JavaScript Basics Variables and Flow Control/016 Advanced If Statements/script.js
+++ 03 JavaScript Basics Variables and Flow Control/016 Advanced If Statements/script.js
@@ -1,2 +1,18 @@
 'use strict';
-let isAccountLocked;
+let isAccountLocked = false;
+let userRole = 'admin';
+userRole = 'user';
+
+if (isAccountLocked) {
+  console.log('Account Status1\nIs account locked');
+} else {
+  console.log('Account Status1\nWelcome!');
+}
+
+if (isAccountLocked) {
+  console.log('Account Status2\nIs account locked');
+} else if (userRole === 'admin') {
+  console.log('Account Status2\nWelcome Admin');
+} else {
+  console.log('Account Status2\nWelcome user');
+}
