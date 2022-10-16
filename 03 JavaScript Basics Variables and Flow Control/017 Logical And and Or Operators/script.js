efec07d4768392caf36aef34f6a3c69649c19a5d
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-10-16 19:36:51 -0400

    updating 017 Logical And and Or Operators,CodeGamingDesk 10/16/2022 #3

--- 03 JavaScript Basics Variables and Flow Control/017 Logical And and Or Operators/script.js
+++ 03 JavaScript Basics Variables and Flow Control/017 Logical And and Or Operators/script.js
@@ -10,3 +10,20 @@ if (temp >= 60 && temp <= 90) {
 } else {
   console.log('Eh. Do what you want');
 }
+
+/// Challenge Area
+
+let isGuestOneVegan = false;
+let isGuestTwoVegan = false;
+
+/// Are both vegan? Only offer up vegan dishes.
+/// At least one vegan? Make sure to offer up some vegan options.
+/// Else, offer up anything on the menu
+
+if (isGuestOneVegan && isGuestTwoVegan) {
+  console.log('Vegan Q and A\nOnly offer up vegan dishes');
+} else if (isGuestOneVegan || isGuestTwoVegan) {
+  console.log('Vegan Q and A\nMake sure to offer up some vegan options');
+} else {
+  console.log('Vegan Q and A\noffer up anything on the menu');
+}
