0b00509c5a2fe03211ef0f35fb5edb2808d1c3a2
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2022-10-16 16:05:12 -0400

    finished 016 Advanced If statements, MacBookPro 10/16/2022

--- 03 JavaScript Basics Variables and Flow Control/016 Advanced If Statements/script.js
+++ 03 JavaScript Basics Variables and Flow Control/016 Advanced If Statements/script.js
@@ -16,3 +16,20 @@ if (isAccountLocked) {
 } else {
   console.log('Account Status2\nWelcome user');
 }
+
+/// Challenge Area
+let temp = 121;
+
+/// It is freezing outside!
+/// It is hot outside!
+/// Go for it. It is pretty nice.
+
+if (temp <= 31) {
+  console.log('Temp status\nIt is freezing outside!');
+} else if (temp <= 45) {
+  console.log('Temp status\nGo for it. It is pretty nice');
+} else if (temp <= 110) {
+  console.log('Temp status\nIt is hot outside!');
+} else {
+  console.log('Temp status\nIt is blazing hot outside!');
+}
